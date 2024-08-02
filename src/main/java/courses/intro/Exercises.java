77e1eb9eab1fb20b95118921e8aef5bf378962fc
Alex Mihaila <alexmihaila93@gmail.com>
2024-08-02 13:51:31 +0300

    java

---
+++ src/main/java/courses/intro/Exercises.java
@@ -0,0 +1,4 @@
+package courses.intro;
+
+public class Exercises {
+}
--- src/main/test/MyTestNew.java
+++ src/main/test/MyTestNew.java
@@ -1,3 +1,10 @@
 public class MyTestNew {
-    // new file for testing git
+
+    public static void main(String[] args) {
+
+        System.out.println("hello world");
+
+
+    }
+
 }
